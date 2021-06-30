d4ee26a0a038e524cdb81771dc83c6f54015d20d
devstik0407 <swastik713@gmail.com>
2021-06-30 10:10:00 +0530

    spec for download from url with path parameters only

---
+++ utils/download.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func DownloadAndSave(src, dest string) (string, error) {
+	res, err := http.Get(src)
+	if err != nil {
+		return "", err
+	}
+
+	u, err := url.Parse(src)
+	if err != nil {
+		return "", err
+	}
+
+	pathSplit := strings.Split(u.Path, "/")
+	fileName := pathSplit[len(pathSplit)-1]
+
+	fileNameSplit := strings.Split(filepath.Join(dest, fileName), ".")
+	if len(fileNameSplit) < 2 {
+		return "", errors.New("invalid url for downloading file")
+	}
+	ext := fileNameSplit[1]
+
+	_, err = os.Stat(fileName)
+	for !os.IsNotExist(err) {
+		fmt.Printf("A file with same name %s already exists in the directory", fileName)
+		fmt.Println("Enter a name (without extension) for the file:")
+		newName := ""
+		fmt.Scanln(&newName)
+		fileName = newName + "." + ext
+		_, err = os.Stat(fileName)
+	}
+
+	f, err := os.Create(filepath.Join(dest, fileName))
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, res.Body)
+	if err != nil {
+		return "", err
+	}
+	return fileName, nil
+}
---
+++ utils/download_test.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestURLWithOnlyPath(t *testing.T) {
+	src := "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
+	dest, err := os.Getwd()
+	if err != nil {
+		t.Fatal("Error in getting destination")
+	}
+
+	fileName, err := DownloadAndSave(src, dest)
+	if err != nil {
+		t.Fatal("Download failed")
+	}
+
+	if _, err = os.Stat(filepath.Join(dest, fileName)); os.IsNotExist(err) {
+		t.Fatal("Download failed")
+	}
+}
