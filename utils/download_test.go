2e88e1b429c5def3e58b52070a07796e4217e1a9
devstik0407 <swastik713@gmail.com>
2021-06-30 10:15:14 +0530

    spec for download from url with query parameters also

--- utils/download_test.go
+++ utils/download_test.go
@@ -22,3 +22,20 @@ func TestURLWithOnlyPath(t *testing.T) {
 		t.Fatal("Download failed")
 	}
 }
+
+func TestURLWithQueryParams(t *testing.T) {
+	src := "https://homepages.cae.wisc.edu/~ece533/images/boat.png?q=10&fft=101"
+	dest, err := os.Getwd()
+	if err != nil {
+		t.Fatal("Error in getting destination")
+	}
+
+	fileName, err := DownloadAndSave(src, dest)
+	if err != nil {
+		t.Fatal("Download failed")
+	}
+
+	if _, err = os.Stat(filepath.Join(dest, fileName)); os.IsNotExist(err) {
+		t.Fatal("Download failed")
+	}
+}
